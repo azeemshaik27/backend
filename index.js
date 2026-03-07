225636d126a1df6fbbfd2a0640f19e020db50682
nairx <nairxprv@gmail.com>
2026-03-07 09:19:33 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
-import {productRouter} from "./routes/productRoute.js";
+// import {productRouter} from "./routes/productRoute.js";
+import { storeRouter } from "./routes/storeRoute.js";
 const app = express();
+app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.use(expressLayouts);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -17,10 +18,10 @@ app.use(
   }),
 );
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.use("/products", productRouter);
+app.use("/", storeRouter);
+// app.use("/auth", authRouter);
+// app.use("/products", productRouter);
+// app.use("/users", userRouter);
 
 app.listen(5000, () => {
   console.log("Server Started");
--- routes/productRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import { showProducts } from "../controllers/productController.js";
-const productRouter = express.Router()
-
-productRouter.get("/",showProducts)
-
-export {productRouter}
